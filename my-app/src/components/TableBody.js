c3978bd30dd366ebb6054abf47d73dc1def9dbb9
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-08 18:50:49 -0500

    Finish TableBody

--- my-app/src/components/TableBody.js
+++ my-app/src/components/TableBody.js
@@ -4,8 +4,18 @@ function TableBody (props) {
     return (
         <tbody>
             {props.users.map((user) => {
-                return ()
+                return (
+                    <tr key={user.login.uuid}>
+                    <td><img src={user.picture.medium} /></td>
+                    <td>{user.name.first}</td>
+                    <td>{user.name.last}</td>
+                    <td>{user.email}</td>
+                    <td>{user.phone}</td>
+                </tr> 
+                )
             })}
         </tbody>
     )
 }
+
+export default TableBody;
