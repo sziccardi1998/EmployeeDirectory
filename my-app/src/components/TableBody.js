a884526ae2743da95938f9f3d26d617b58f537d8
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-08 18:49:42 -0500

    beggining structure of tablebody

--- my-app/src/components/TableBody.js
+++ my-app/src/components/TableBody.js
@@ -1,2 +1,11 @@
 import React from "react";
 
+function TableBody (props) {
+    return (
+        <tbody>
+            {props.users.map((user) => {
+                return ()
+            })}
+        </tbody>
+    )
+}
