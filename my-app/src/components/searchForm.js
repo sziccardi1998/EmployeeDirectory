d85a0c9d10baed476c27360fa7693975849240bb
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-08 19:15:18 -0500

    fill out search form.

--- my-app/src/components/searchForm.js
+++ my-app/src/components/searchForm.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function SearchForm () {
     return (
-
+        <form className="search">
+        <div className="form-group mb-2">
+          <label htmlFor="name">&nbsp; &nbsp; First Name: &nbsp; </label>
+          <input
+            placeholder="Type a name to begin"
+          />
+        &nbsp; &nbsp; <button type="submit" className="btn btn-primary mb-2">Search</button>
+        </div>
+        </form>
+  
     )
 }
 
