911b81d2ff87eca1683687778fb8a5a01d64f8e3
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-08 19:11:41 -0500

    additions to headerinfo

--- my-app/src/components/headerInfo.js
+++ my-app/src/components/headerInfo.js
@@ -2,7 +2,14 @@ import "./styles.css";
 import React from "react";
 
 function HeaderInfo() {
-
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Employee Directory</h1>
+                <p className="lead">List of Company Employees</p>
+            </div>
+        </div>
+    );
 }
 
 export default HeaderInfo;
